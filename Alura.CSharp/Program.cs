bc9c5e74f21d46a7274988eb9e7bd31f5e311683
SARA L. SOUZA <108760378+sara-larissa@users.noreply.github.com>
2022-11-23 15:43:29 -0300

    Adc. pastas

---
+++ Alura.CSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, mundo");
+            Console.WriteLine("Digite ok e ENTER, para continuar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Criando variáveis.Flutuante/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criando_variáveis.Flutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 - Variáveis ponto flutuante");
+
+            double salario; //variável  double aceita int, mas int não aceita double
+            //salario = 3000.10;
+
+            double idade; 
+            idade = 7/5; //para apresentar n° double tem que colocar 7.0 se não ele faz a divisão e desconsidera depois da virgula
+
+            Console.WriteLine(salario);
+
+            Console.WriteLine("Tecle enter para continuar");
+            Console.ReadLine();
+        }
+    }
+}
